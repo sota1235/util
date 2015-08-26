8569616846defc8729eea93a21d506f47211fd4f
sota1235 <sota1235@gmail.com>
2015-08-26 13:31:11 +0900

    add: 配列を木構造に変換するphpスクリプトを追加

---
+++ php/tree.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * 部署ツリー作成用スクリプト
+ */
+
+function makeTree($tree, $parintId, $orgs)
+{
+    for($i=0;$i<count($orgs);) {
+        $org = $orgs[$i];
+        if($org[2] == $parintId) {
+            $child = [
+                $org[1] => [
+                    'id'    => $org[0],
+                    'child' => []
+                ]
+            ];
+            array_splice($orgs, $i, 1);
+            //array_push($child[$org[1]]['child'], makeTree([], $org[0], $orgs));
+            //array_push($tree, $child);
+            $child[$org[1]]['child'] += makeTree([], $org[0], $orgs);
+            $tree += $child;
+        } else {
+            $i++;
+        }
+    }
+    return $tree;
+}
+
+$testCase = [
+    [1, "おいしんぼ", null],
+    [2, "おいしん",   1],
+    [3, "おいんし",   1],
+    [4, "おいし",     2],
+    [5, "おしい",     2],
+    [6, "おし",       4],
+    [7, "さぼるなよ", null],
+    [8, "さぼるな",   7],
+    [9, "さぼなる",   7],
+    [10,"さぼる",     8]
+];
+
+echo "<pre>";
+echo json_encode(makeTree([], null, $testCase));
+echo "</pre>";
