e6249ba2bb9f848e1a0a982ad02db42d8892a23e
sota1235 <sota1235@gmail.com>
2015-08-28 10:23:32 +0900

    fix: continue when  is not child

--- php/tree.php
+++ php/tree.php
@@ -2,27 +2,28 @@
 /**
  * 部署ツリー作成用スクリプト
  */
-
-function makeTree($parintId, $orgs)
+function makeTree($parentId, $orgs)
 {
     $tree = [];
     for($i=0;$i<count($orgs);) {
         $org = $orgs[$i];
-        if($org[2] == $parintId) {
-            $child = [
-                $org[1] => [
-                    'id'    => $org[0],
-                    'child' => []
-                ]
-            ];
-            array_splice($orgs, $i, 1);
-            //array_push($child[$org[1]]['child'], makeTree([], $org[0], $orgs));
-            //array_push($tree, $child);
-            $child[$org[1]]['child'] += makeTree($org[0], $orgs);
-            $tree += $child;
-        } else {
+        if($org[2] != $parentId) {
             $i++;
+            continue;
         }
+        // 子要素として追加するための配列生成
+        $child = [
+            $org[1] => [
+                'id'    => $org[0],
+                'child' => []
+            ]
+        ];
+        // 子部署候補から削除
+        array_splice($orgs, $i, 1);
+        // さらに子供を再帰で追加
+        $child[$org[1]]['child'] += makeTree($org[0], $orgs);
+        // 全ての子供を抱えた状態で$treeに連結
+        $tree += $child;
     }
     return $tree;
 }
